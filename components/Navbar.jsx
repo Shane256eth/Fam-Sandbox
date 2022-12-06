abc8bd3a8f20ed3f36ac4f5e8b44236ee7ee440b
Shane <racer9090@yahoo.com>
2022-12-05 23:44:38 -0800

    fr

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,23 +1,22 @@
-"use client";
+'use client';
 
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 
-import styles from "../styles";
-import { navVariants } from "../utils/motion";
+import { navVariants } from '../utils/motion';
 
 const Navbar = () => (
   <motion.nav
     variants={navVariants}
     initial="hidden"
     whileInView="show"
-    className={"${styles.xPaddings} py-8 relative"}
+    className="$styles.xPaddings py-8 relative"
   >
     <div className="absolute w-[50%] inset-0 gradient-01" />
-    <div className={"${styles.innerWidth} mx-auto flex justify-between gap-8"}>
+    <div className="$styles.innerWidth mx-auto flex justify-between gap-8">
       <img
         src="/search.svg"
         alt="search"
-        classname="w-[24px] h-[24px] object-contain"
+        className="w-[24px] h-[24px] object-contain"
       />
 
       <h2 className="font-extrabold text-[24px] leading-[30px] text-white">
@@ -26,7 +25,7 @@ const Navbar = () => (
       <img
         src="/menu.svg"
         alt="menu"
-        classname="w-[24px] h-[24px] object-contain"
+        className="w-[24px] h-[24px] object-contain"
       />
     </div>
   </motion.nav>
