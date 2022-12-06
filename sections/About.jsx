f62a45f8c3b86d36ec2de8737c083839613ec3b9
Shane <racer9090@yahoo.com>
2022-12-06 12:08:30 -0800

    sa

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,30 +1,31 @@
-"use client";
+'use client';
 
-import { motion } from "framer-motion";
-import { TypingText } from "../components";
+import { motion } from 'framer-motion';
+import { TypingText } from '../components';
 
-import styles from "../styles";
-import { fadeIn, staggerContainer, textVariant } from "../utils/motion";
+import styles from '../styles';
+import { fadeIn, staggerContainer, textVariant } from '../utils/motion';
 
 const About = () => (
-  <section className={"${styles.paddings} relative z-10"}>
+  <section className={'${styles.paddings} relative z-10'}>
     <div className="gradient-02 z-0" />
     <motion.div
       variants={staggerContainer}
       initial="hidden"
       whileInView="show"
       viewport={{ once: false, amount: 0.25 }}
-      className={"${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col"}
+      className={'${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col'}
     >
-      <TypingText title="| About Metaversus" textStyles="text-center" />
+      <TypingText title="| About the Fam" textStyles="text-center" />
 
       <motion.p
-        variants={fadeIn("down", "tween", 2.2, 1)}
+        variants={fadeIn('down', 'tween', 2.2, 1)}
         className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
       >
-        Welcome to the BananaFam headquarters. Where you can find out all the
-        newest things happening withus, BananaBeatz, as well as merch store,
-        DAO, minting NFTs, and more!
+        Welcome to the BananaFam headquarters... where you can find out all the
+        newest things happening with The Fam, check out BananaBeatz, as well as
+        the Merch, minting NFTs, paying with Peanutz and upcoming dynamic NFTs
+        and DAO!
       </motion.p>
     </motion.div>
   </section>
