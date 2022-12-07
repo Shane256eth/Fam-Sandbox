80238f8252eb6cd4280b9e2f2aa4bd0c03cb2523
Shane <racer9090@yahoo.com>
2022-12-07 12:39:51 -0800

    g

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,9 @@
+import { Contract, providers, utils } from 'ethers';
+import React, { useEffect, useRef, useState } from 'react';
+import Web3Modal from 'web3modal';
+import { abi, NFT_CONTRACT_ADDRESS } from '../constants';
+import styles from '../styles/Home.module.css';
+
 import { Footer, Navbar } from '../components';
 import {
   About,
@@ -10,13 +16,6 @@ import {
   World,
 } from '../sections';
 
-import { Contract, providers, utils } from 'ethers';
-import Head from 'next/head';
-import React, { useEffect, useRef, useState } from 'react';
-import Web3Modal from 'web3modal';
-import { abi, NFT_CONTRACT_ADDRESS } from '../constants';
-import styles from '../styles/Home.module.css';
-
 export default function Home() {
   // walletConnected keep track of whether the user's wallet is connected or not
   const [walletConnected, setWalletConnected] = useState(false);
@@ -26,6 +25,38 @@ export default function Home() {
   const [tokenIdsMinted, setTokenIdsMinted] = useState('0');
   // Create a reference to the Web3 Modal (used for connecting to Metamask) which persists as long as the page is open
   const web3ModalRef = useRef();
+
+  /**
+   * Returns a Provider or Signer object representing the Ethereum RPC with or without the
+   * signing capabilities of metamask attached
+   *
+   * A `Provider` is needed to interact with the blockchain - reading transactions, reading balances, reading state, etc.
+   *
+   * A `Signer` is a special type of Provider used in case a `write` transaction needs to be made to the blockchain, which involves the connected account
+   * needing to make a digital signature to authorize the transaction being sent. Metamask exposes a Signer API to allow your website to
+   * request signatures from the user using Signer functions.
+   *
+   * @param {*} needSigner - True if you need the signer, default false otherwise
+   */
+  const getProviderOrSigner = async (needSigner = false) => {
+    // Connect to Metamask
+    // Since we store `web3Modal` as a reference, we need to access the `current` value to get access to the underlying object
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+
+    // If user is not connected to the Mumbai network, let them know and throw an error
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 80001) {
+      window.alert('Change the network to Mumbai');
+      throw new Error('Change network to Mumbai');
+    }
+
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+    return web3Provider;
+  };
 
   /**
    * publicMint: Mint an NFT
@@ -82,43 +113,11 @@ export default function Home() {
       // call the tokenIds from the contract
       const _tokenIds = await nftContract.tokenIds();
       console.log('tokenIds', _tokenIds);
-      //_tokenIds is a `Big Number`. We need to convert the Big Number to a string
+      // _tokenIds is a `Big Number`. We need to convert the Big Number to a string
       setTokenIdsMinted(_tokenIds.toString());
     } catch (err) {
       console.error(err);
     }
-  };
-
-  /**
-   * Returns a Provider or Signer object representing the Ethereum RPC with or without the
-   * signing capabilities of metamask attached
-   *
-   * A `Provider` is needed to interact with the blockchain - reading transactions, reading balances, reading state, etc.
-   *
-   * A `Signer` is a special type of Provider used in case a `write` transaction needs to be made to the blockchain, which involves the connected account
-   * needing to make a digital signature to authorize the transaction being sent. Metamask exposes a Signer API to allow your website to
-   * request signatures from the user using Signer functions.
-   *
-   * @param {*} needSigner - True if you need the signer, default false otherwise
-   */
-  const getProviderOrSigner = async (needSigner = false) => {
-    // Connect to Metamask
-    // Since we store `web3Modal` as a reference, we need to access the `current` value to get access to the underlying object
-    const provider = await web3ModalRef.current.connect();
-    const web3Provider = new providers.Web3Provider(provider);
-
-    // If user is not connected to the Mumbai network, let them know and throw an error
-    const { chainId } = await web3Provider.getNetwork();
-    if (chainId !== 80001) {
-      window.alert('Change the network to Mumbai');
-      throw new Error('Change network to Mumbai');
-    }
-
-    if (needSigner) {
-      const signer = web3Provider.getSigner();
-      return signer;
-    }
-    return web3Provider;
   };
 
   // useEffects are used to react to changes in state of the website
@@ -177,9 +176,6 @@ export default function Home() {
 
       <Hero />
       <div>
-        <h1 className={styles.title}></h1>
-        <div className={styles.description}></div>
-
         <div className="text-secondary-white">
           {tokenIdsMinted}/10 have been minted
         </div>
@@ -195,9 +191,6 @@ export default function Home() {
         <GetStarted />
         <div className="gradient-04 z-0" />
         <div>
-          <h1 className={styles.title}></h1>
-          <div className={styles.description}></div>
-
           <div className="text-secondary-white">
             {tokenIdsMinted}/10 have been minted
           </div>
