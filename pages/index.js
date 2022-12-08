e96e0d643cf5a69ead355e0ff3238b2c069ed682
Shane <racer9090@yahoo.com>
2022-12-08 10:42:29 -0800

    d

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ import {
   World,
 } from '../sections';
 
-export default function Home() {
+const Home = () => {
   // walletConnected keep track of whether the user's wallet is connected or not
   const [walletConnected, setWalletConnected] = useState(false);
   // loading is set to true when we are waiting for a transaction to get mined
@@ -26,18 +26,6 @@ export default function Home() {
   // Create a reference to the Web3 Modal (used for connecting to Metamask) which persists as long as the page is open
   const web3ModalRef = useRef();
 
-  /**
-   * Returns a Provider or Signer object representing the Ethereum RPC with or without the
-   * signing capabilities of metamask attached
-   *
-   * A `Provider` is needed to interact with the blockchain - reading transactions, reading balances, reading state, etc.
-   *
-   * A `Signer` is a special type of Provider used in case a `write` transaction needs to be made to the blockchain, which involves the connected account
-   * needing to make a digital signature to authorize the transaction being sent. Metamask exposes a Signer API to allow your website to
-   * request signatures from the user using Signer functions.
-   *
-   * @param {*} needSigner - True if you need the signer, default false otherwise
-   */
   const getProviderOrSigner = async (needSigner = false) => {
     // Connect to Metamask
     // Since we store `web3Modal` as a reference, we need to access the `current` value to get access to the underlying object
@@ -120,6 +108,19 @@ export default function Home() {
     }
   };
 
+  /**
+   * Returns a Provider or Signer object representing the Ethereum RPC with or without the
+   * signing capabilities of metamask attached
+   *
+   * A `Provider` is needed to interact with the blockchain - reading transactions, reading balances, reading state, etc.
+   *
+   * A `Signer` is a special type of Provider used in case a `write` transaction needs to be made to the blockchain, which involves the connected account
+   * needing to make a digital signature to authorize the transaction being sent. Metamask exposes a Signer API to allow your website to
+   * request signatures from the user using Signer functions.
+   *
+   * @param {*} needSigner - True if you need the signer, default false otherwise
+   */
+
   // useEffects are used to react to changes in state of the website
   // The array at the end of function call represents what state changes will trigger this effect
   // In this case, whenever the value of `walletConnected` changes - this effect will be called
@@ -172,16 +173,17 @@ export default function Home() {
   return (
     <div className="bg-primary-black overflow-hidden">
       <Navbar />
-
       <Hero />
-      <div>
-        <div className="text-secondary-white">
-          {tokenIdsMinted}/10 have been minted
-        </div>
-        {renderButton()}
-      </div>
-
       <div className="relative">
+        <div>
+          <div className={styles.description}>
+            Its an NFT collection for the entire fam.
+          </div>
+          <div className={styles.description}>
+            {tokenIdsMinted}/10 have been minted
+          </div>
+          {renderButton()}
+        </div>
         <About />
         <div className="gradient-03 z-0" />
         <Explore />
@@ -189,12 +191,6 @@ export default function Home() {
       <div className="relative">
         <GetStarted />
         <div className="gradient-04 z-0" />
-        <div>
-          <div className="text-secondary-white">
-            {tokenIdsMinted}/10 have been minted
-          </div>
-          {renderButton()}
-        </div>
         <WhatsNew />
       </div>
       <World />
@@ -206,4 +202,6 @@ export default function Home() {
       <Footer />
     </div>
   );
-}
+};
+
+export default Home;
