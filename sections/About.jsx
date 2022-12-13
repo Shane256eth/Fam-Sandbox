ee2cf849a5b8604f19b2a82824ab06c5fc884370
Shane <racer9090@yahoo.com>
2022-12-13 09:40:03 -0800

    g

--- sections/About.jsx
+++ sections/About.jsx
@@ -11,31 +11,73 @@ const About = () => (
     <div className="gradient-02 z-0" />
     <motion.div
       variants={staggerContainer}
+      className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
       initial="hidden"
       whileInView="show"
       viewport={{ once: false, amount: 0.25 }}
-      className={'${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col'}
     >
-      <TypingText title="| About the Fam" textStyles="text-center" />
-
       <motion.p
-        variants={fadeIn('down', 'tween', 1.6, 1)}
+        variants={fadeIn('down', 'tween', 0.1, 0.1)}
         className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+        whileHover={{
+          scale: [1.8, 5.5, 2.5],
+          rotate: [0, 10, -10, 10, 0],
+        }}
       >
         <span className="font-bold text-secondary-yellow">
           Welcome to <br></br> BananaFam HQ
-        </span>
-        <br></br>where you <br></br> can find out what's new <br></br>
-        with the crew<br></br>
-        <br></br>
+        </span>{' '}
+      </motion.p>
+      <br></br>where you <br></br> can find out what's new <br></br>
+      with the crew<br></br>
+      <br></br>
+      <motion.p
+        variants={fadeIn('down', 'tween', 0.6, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+        whileHover={{
+          scale: [1, 3.5, 1.4],
+          rotate: [0, 10, -10, 10, 0],
+        }}
+      >
         <span className="font-bold text-secondary-orange">
           Merch
           <br></br> Streaming <br></br> BananaBeatz <br></br>The NFT factory
           <br></br> The Peanutz Vault <br></br>
         </span>
-        <br></br>..watch out for <b>Dynamic NFTs</b>
-        <br></br>and <b>The DAO!</b>
       </motion.p>
+      <br></br>..watch out for <b>Dynamic NFTs</b>
+      <br></br>and <b>The DAO!</b>
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={{
+          hidden: {
+            scale: 0.2,
+            opacity: 0,
+          },
+          visible: {
+            scale: 0.2,
+            opacity: 0.2,
+          },
+        }}
+        whileHover={{
+          scale: [0.5, 1, 0.5],
+          rotate: [0, 10, -10, 10, 0],
+          opacity: 1,
+          filter: [
+            'hue-rotate(0) contrast(100%)',
+            'hue-rotate(360deg) contrast(200%)',
+            'hue-rotate(45deg) contrast(300%)',
+            'hue-rotate(0) contrast(100%)',
+          ],
+        }}
+      >
+        <img
+          src="/bananas.svg"
+          alt="get-started"
+          className="w-[90%] h-[90%] object-contain"
+        />
+      </motion.div>
     </motion.div>
   </section>
 );
